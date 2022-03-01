b3a986daae258fe495e3c37c618a98c5123eda2f
Xiaoyu Chen <xchen@irthsolutions.com>
2022-02-28 20:00:15 -0500

    Rotate the old way

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,17 +9,23 @@ namespace ZhengJesse.Lab3
     public class CameraControl : MonoBehaviour
     {
         [SerializeField]
-        private Transform target;
+        private Transform _Character;
 
-        [SerializeField] private Vector3 offset;
+        [SerializeField] private Vector3 _CameraCharacterOffset;
+
+        [SerializeField]
+        public float _mouseSensitivity = 30f;
+
+
         public float smoothSpeed = 0.125f;
 
-        public float speed = 50f;
-        public float sensitivity = 10f;
+        public float _speed = 50f;
 
-        private bool characterMoved = false;
+        private bool _Initialized = false;
 
-        private int count = 0;
+        private float _rotationY;
+        private float _rotationX;
+
 
         private DateTime? noMouseMovementStartTime;
 
@@ -37,31 +43,47 @@ namespace ZhengJesse.Lab3
 
             UnityEngine.Debug.Log($"delta.magnitude: {delta.magnitude}");
 
+            if(!_Initialized)
+            {
+                FollowTarget();
+                _Initialized = true;
+
+                return;
+            }
+
             if (delta.magnitude == 0.0)
             {
-                if(!noMouseMovementStartTime.HasValue)
+                if (!noMouseMovementStartTime.HasValue)
                     noMouseMovementStartTime = DateTime.Now;
-
-                TimeSpan tp = DateTime.Now - noMouseMovementStartTime.Value;
-
-                if(tp.TotalMilliseconds>200)
-                    FollowTarget();
             }
             else
             {
                 noMouseMovementStartTime = null;
-                RotateToMousePosition();
             }
+            if (noMouseMovementStartTime.HasValue)
+            {
+                TimeSpan tp = DateTime.Now - noMouseMovementStartTime.Value;
+
+                if (tp.TotalMilliseconds > 1000)
+                {
+                    FollowTarget();
+                    noMouseMovementStartTime = null;
+                    return;
+                }
+            }
+            else
+                RotateToMousePositionOld();
 
         }
         private void FollowTarget()
         {
-            if (target == null)
+            if (_Character == null)
                 return;
-            Vector3 desiredPos = target.position + offset;
+            Vector3 desiredPos = _Character.position + _CameraCharacterOffset;
             transform.position = desiredPos;
-            transform.LookAt(target);
+            transform.LookAt(_Character);
         }
+        
         private void RotateToMousePosition()
         {
             Vector3 mousePos = Mouse.current.position.ReadValue();
@@ -69,11 +91,48 @@ namespace ZhengJesse.Lab3
 
             UnityEngine.Debug.Log($"new system: {mousePos.x}. {Worldpos.x}");
 
-            var step = speed * Time.deltaTime;
+            var step = _speed * Time.deltaTime;
 
-            Quaternion rot = Quaternion.Euler(mousePos.x, mousePos.y, mousePos.y);
+            Quaternion rot = Quaternion.Euler(Worldpos.x, Worldpos.y, Worldpos.y);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, step);
         }
+        
+        private void RotateToMousePositionOld()
+        {
+            float mouseX = UnityEngine.Input.GetAxis("Mouse X")* _mouseSensitivity;
+            float mouseY = UnityEngine.Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+            Vector3 worldpos = Camera.allCameras[0].ScreenToWorldPoint(mousePos);
+
+
+            _rotationX += mouseY;
+            _rotationY += mouseX;
+
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+
+        /*
+        private void RotateToMousePosition()
+        {
+            //return;
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+            Vector3 Worldpos = Camera.allCameras[0].ScreenToWorldPoint(mousePos);
+            var step = speed * Time.deltaTime;
+
+            Quaternion rot = Quaternion.LookRotation(Vector3.forward, Worldpos - transform.position);
+            rot = Quaternion.Euler(rot.x,rot.y, -rot.z-45);
+            UnityEngine.Debug.Log(rot);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, step);
+
+
+
+            //Quaternion rot = Quaternion.Euler(mousePos.x, mousePos.y, mousePos.y);
+
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, step);
+        }
+        */
     }
 }
